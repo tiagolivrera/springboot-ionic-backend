4ed3c83228b76ba002abbcc190e19aeb14be55ff
Thyago Freitas <thyago.olifreitas@gmail.com>
2022-02-04 12:05:37 -0300

    Add Categoria domain class

--- src/main/java/com/tiagolivrera/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/tiagolivrera/cursomc/resources/CategoriaResource.java
@@ -1,5 +1,10 @@
 package com.tiagolivrera.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tiagolivrera.cursomc.domain.Categoria;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
     
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "REST está funcionando!";
+    public List<Categoria> listar() {
+        
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
