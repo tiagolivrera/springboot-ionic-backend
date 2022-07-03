8f633173b415432f63a06d92aebca04a074b1761
tiagolivrera <thyago.olifreitas@gmail.com>
2022-07-03 18:34:34 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/tiagolivrera/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/tiagolivrera/cursomc/resources/CategoriaResource.java
@@ -3,12 +3,16 @@ package com.tiagolivrera.cursomc.resources;
 import com.tiagolivrera.cursomc.domain.Categoria;
 import com.tiagolivrera.cursomc.services.CategoriaService;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -21,5 +25,13 @@ public class CategoriaResource {
     public ResponseEntity<?> find(@PathVariable Integer id) { // id passado via endpoint, usa @PathVariable
         Categoria obj = service.find(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) { // @RequestBody -- converte de JSON para Java automaticamente
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri(); // criando uma nova url com o id do objeto inserido
+        return ResponseEntity.created(uri).build(); // retorna a url com o codigo 201 (created) -- insercao de dado
     }
 }
--- src/main/java/com/tiagolivrera/cursomc/services/CategoriaService.java
+++ src/main/java/com/tiagolivrera/cursomc/services/CategoriaService.java
@@ -18,7 +18,12 @@ public class CategoriaService {
     public Categoria find(Integer id) {
         Optional<Categoria> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
-            "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()
-        ));
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
+
+    public Categoria insert(Categoria obj) {
+        obj.setId(null); // garantindo que o objeto a ser inserido é novo
+        return repository.save(obj); // se id for diferente de null, save() vai tratar o obj como uma atualizacao, e
+                                     // nao uma insercao
     }
 }
