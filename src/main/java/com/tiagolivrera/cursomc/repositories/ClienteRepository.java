d23d94aec0653d574fd0b61ff9b233f090bd8ae2
tiagolivrera <thyago.olifreitas@gmail.com>
2022-07-18 16:59:53 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/tiagolivrera/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/tiagolivrera/cursomc/repositories/ClienteRepository.java
@@ -4,8 +4,16 @@ import com.tiagolivrera.cursomc.domain.Cliente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-    
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    @Transactional(readOnly = true) /*
+                                     * readonly=true -- operacao nao necessita de ser envolvida como uma transacao
+                                     * do banco de dados, ficando mais rapido e diminuindo o locking no
+                                     * gerenciamento de operacoes do banco de dados
+                                     */
+    Cliente findByEmail(String email); // o Spring Data implementa esse metodo automaticamente
+
 }
--- src/main/java/com/tiagolivrera/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/tiagolivrera/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tiagolivrera.cursomc.domain.Cliente;
 import com.tiagolivrera.cursomc.domain.enums.TipoCliente;
 import com.tiagolivrera.cursomc.dto.ClienteNewDTO;
+import com.tiagolivrera.cursomc.repositories.ClienteRepository;
 import com.tiagolivrera.cursomc.resources.exceptions.FieldMessage;
 import com.tiagolivrera.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @Override
     public void initialize(ClienteInsert ann) {
@@ -28,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+        if(aux != null) { // email ja existe no BD (logica de negocio: email unico)
+            list.add(new FieldMessage("email", "Email já existente"));
         }
 
         for (FieldMessage e : list) {
