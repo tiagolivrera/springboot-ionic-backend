0700239d28e3539d2fb5eb64a402cfaaf916a549
tiagolivrera <thyago.olifreitas@gmail.com>
2022-07-20 15:50:57 -0300

    Criando o profile de desenvolvimento

---
+++ src/main/java/com/tiagolivrera/cursomc/config/DevConfig.java
@@ -0,0 +1,39 @@
+package com.tiagolivrera.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.tiagolivrera.cursomc.services.DBService;
+
+@Component
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+
+        if (!"create".equals(strategy)) {
+            return false;
+        }
+        
+        dbService.instantiateTestDatabase();
+        return true;
+    }    
+}
+
+/*
+ * OBS.: Mudança na criação de profiles a partir da versão 2.4 do Spring Boot
+ * Acrescentar o profile dentro de spring-boot-maven-plugin em pom.xml
+ * Consulte: https://www.baeldung.com/spring-profiles
+ */
