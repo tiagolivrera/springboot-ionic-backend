84f8b35794d67fc366eda84f9aabb256bd89488f
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2022-07-22 18:10:16 -0300

    Email HTML

--- src/main/java/com/tiagolivrera/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/tiagolivrera/cursomc/services/SmtpEmailService.java
@@ -1,16 +1,22 @@
 package com.tiagolivrera.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 
     @Autowired
     private MailSender mailSender; // o framework instancia automaticamente um MailSender com os dados em
                                    // application-dev.properties
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 
@@ -21,4 +27,10 @@ public class SmtpEmailService extends AbstractEmailService {
         LOG.info("Email enviado");
     }
 
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+        LOG.info("Enviando email...");
+        javaMailSender.send(msg);
+        LOG.info("Email enviado");        
+    }
 }
