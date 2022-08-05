976277195e14bf6eb8bb78050972931660f9dc72
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2022-08-05 10:01:58 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/tiagolivrera/cursomc/CursomcApplication.java
+++ src/main/java/com/tiagolivrera/cursomc/CursomcApplication.java
@@ -1,11 +1,17 @@
 package com.tiagolivrera.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tiagolivrera.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -13,5 +19,6 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("/home/thyago/Documentos/Spring/cursomc/src/main/resources/images/computador.jpg");
 	}
 }
