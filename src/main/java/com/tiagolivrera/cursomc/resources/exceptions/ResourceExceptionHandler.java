ac03581008b6d1b7c5d369722962ce884902c079
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2022-08-18 12:41:20 -0300

    Pradronizando formato das excecoes

--- src/main/java/com/tiagolivrera/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/tiagolivrera/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -23,15 +23,16 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 
-        StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Not found",
+                e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
 
     @ExceptionHandler(DataIntegrityException.class)
     public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 
-        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
-                System.currentTimeMillis());
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "Data integrity", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
@@ -39,27 +40,28 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> methodArgumentNotValid(MethodArgumentNotValidException e,
             HttpServletRequest request) {
 
-        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação",
-                System.currentTimeMillis());
+        ValidationError err = new ValidationError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Validation error", e.getMessage(), request.getRequestURI());
         for (FieldError x : e.getBindingResult().getFieldErrors()) { // captura a lista de erros presentes em
                                                                      // MethodArgumentNotValidException
             err.addError(x.getField(), x.getDefaultMessage());
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
     }
 
     @ExceptionHandler(AuthorizationException.class)
     public ResponseEntity<StandardError> authorizationException(ObjectNotFoundException e, HttpServletRequest request) {
 
-        StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.FORBIDDEN.value(),
+                "Access denied", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
     }
 
     @ExceptionHandler(FileException.class)
     public ResponseEntity<StandardError> fileException(FileException e, HttpServletRequest request) {
 
-        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
-                System.currentTimeMillis());
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "File error", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
@@ -67,21 +69,24 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> amazonServiceException(AmazonServiceException e, HttpServletRequest request) {
 
         HttpStatus code = HttpStatus.valueOf(e.getErrorCode());
-        StandardError err = new StandardError(code.value(), e.getMessage(), System.currentTimeMillis());
+        StandardError err = new StandardError(System.currentTimeMillis(), code.value(),
+                "Amazon service error", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(code).body(err);
     }
 
     @ExceptionHandler(AmazonClientException.class)
     public ResponseEntity<StandardError> amazonClientException(AmazonClientException e, HttpServletRequest request) {
 
-        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "Amazon client error", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
     @ExceptionHandler(AmazonS3Exception.class)
     public ResponseEntity<StandardError> amazonS3Exception(AmazonS3Exception e, HttpServletRequest request) {
 
-        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "S3 error", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
--- src/main/java/com/tiagolivrera/cursomc/resources/exceptions/StandardError.java
+++ src/main/java/com/tiagolivrera/cursomc/resources/exceptions/StandardError.java
@@ -6,13 +6,25 @@ public class StandardError implements Serializable{
 
     private static final long serialVersionUID = 1L;
     
-    private Integer status;
-    private String msg;
     private Long timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
     
-    public StandardError(Integer status, String msg, Long timestamp) {
+    public StandardError(Long timestamp, Integer status, String error, String message, String path) {
+        this.timestamp = timestamp;
         this.status = status;
-        this.msg = msg;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
         this.timestamp = timestamp;
     }
 
@@ -24,19 +36,27 @@ public class StandardError implements Serializable{
         this.status = status;
     }
 
-    public String getMsg() {
-        return msg;
+    public String getError() {
+        return error;
     }
 
-    public void setMsg(String msg) {
-        this.msg = msg;
+    public void setError(String error) {
+        this.error = error;
     }
 
-    public Long getTimestamp() {
-        return timestamp;
+    public String getMessage() {
+        return message;
     }
 
-    public void setTimestamp(Long timestamp) {
-        this.timestamp = timestamp;
-    }    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }       
 }
