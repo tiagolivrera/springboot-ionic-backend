99f28582c6c4673c0ab9450e397d2a73e2bf8b63
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2022-08-18 09:25:34 -0300

    Ajustando tamanho da imagem

--- src/main/java/com/tiagolivrera/cursomc/services/ImageService.java
+++ src/main/java/com/tiagolivrera/cursomc/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -37,7 +38,8 @@ public class ImageService {
     public BufferedImage pngToJpg(BufferedImage img) {
         BufferedImage jpgImage = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
         jpgImage.createGraphics().drawImage(img, 0, 0, Color.WHITE, null);
-        // Color.WHITE -- alguns PNG tem fundo transparente, entao converte a transparencia para branco
+        // Color.WHITE -- alguns PNG tem fundo transparente, entao converte a
+        // transparencia para branco
         return jpgImage;
     }
 
@@ -49,5 +51,19 @@ public class ImageService {
         } catch (IOException e) {
             throw new FileException("Erro ao ler arquivo");
         }
+    }
+    // Funcao que "cropa" a imagem
+    public BufferedImage cropSquare(BufferedImage sourceImg) {
+        int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+        return Scalr.crop(
+                sourceImg,
+                (sourceImg.getWidth() / 2) - (min / 2),
+                (sourceImg.getHeight() / 2) - (min / 2),
+                min,
+                min);
+    }
+
+    public BufferedImage resize(BufferedImage sourceImg, int size) {
+        return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
     }
 }
